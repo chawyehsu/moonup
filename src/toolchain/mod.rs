7cfa3cab019edb62d9c51ee381c7394ee7915f06
Chawye Hsu <su+git@chawyehsu.com>
2024-07-23 23:20:55 +0800

    fix: Improve installed toolchains lookup
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- src/toolchain/mod.rs
+++ src/toolchain/mod.rs
@@ -1,5 +1,3 @@
-use miette::IntoDiagnostic;
-
 pub mod index;
 pub mod package;
 
@@ -18,37 +16,39 @@ pub fn installed_toolchains() -> miette::Result<Vec<InstalledToolchain>> {
     let toolchains_dir = crate::moonup_home().join("toolchains");
     let default_version = default_toolchain();
 
-    let toolchains = toolchains_dir
-        .read_dir()
-        .into_diagnostic()?
-        .filter_map(std::io::Result::ok)
-        .filter_map(|e| {
-            let path = e.path();
-            let version = path.file_name().map(|n| {
-                let n = n.to_ascii_lowercase();
-                let latest = n == "latest";
-                let default = match default_version.as_deref() {
-                    Some(v) => v == n,
-                    None => false,
-                };
-                let version = match latest {
-                    false => n.to_string_lossy().to_string(),
-                    true => std::fs::read_to_string(path.join("version"))
-                        .ok()
-                        .map(|s| s.trim().to_string())
-                        .unwrap_or_else(|| "latest".to_string()),
-                };
+    let toolchains = match toolchains_dir.read_dir() {
+        Err(e) if e.kind() == std::io::ErrorKind::NotFound => vec![],
+        Err(e) => return Err(miette::miette!(e).wrap_err("Failed to read toolchains directory")),
+        Ok(read_dir) => read_dir
+            .filter_map(std::io::Result::ok)
+            .filter_map(|e| {
+                let path = e.path();
+                let version = path.file_name().map(|n| {
+                    let n = n.to_ascii_lowercase();
+                    let latest = n == "latest";
+                    let default = match default_version.as_deref() {
+                        Some(v) => v == n,
+                        None => false,
+                    };
+                    let version = match latest {
+                        false => n.to_string_lossy().to_string(),
+                        true => std::fs::read_to_string(path.join("version"))
+                            .ok()
+                            .map(|s| s.trim().to_string())
+                            .unwrap_or_else(|| "latest".to_string()),
+                    };
 
-                InstalledToolchain {
-                    default,
-                    latest,
-                    version,
-                }
-            });
+                    InstalledToolchain {
+                        default,
+                        latest,
+                        version,
+                    }
+                });
 
-            version
-        })
-        .collect::<Vec<_>>();
+                version
+            })
+            .collect::<Vec<_>>(),
+    };
 
     Ok(toolchains)
 }
