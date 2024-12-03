ee2fefcbe798b536e80aa3d7587abef93dda4748
Chawye Hsu <su+git@chawyehsu.com>
2024-12-04 01:19:16 +0800

    fix(cli): show command should list active toolchain

--- src/cli/show.rs
+++ src/cli/show.rs
@@ -1,19 +1,34 @@
 use clap::Parser;
 
-/// Show installed and currently active toolchains
+use crate::toolchain::{installed_toolchains, resolve, InstalledToolchain};
+
+/// Show installed and active toolchains
 #[derive(Parser, Debug)]
 pub struct Args {}
 
 pub async fn execute(_: Args) -> miette::Result<()> {
-    let toolchains = crate::toolchain::installed_toolchains()?;
+    let installed = installed_toolchains()?;
 
     println!("Moonup home: {}\n", crate::moonup_home().display());
-    if toolchains.is_empty() {
+    if installed.is_empty() {
         println!("No toolchains installed");
     } else {
+        let default = resolve::detect_default_version();
+
         println!("Installed toolchains:");
-        for toolchain in toolchains {
-            let tag = match (toolchain.default, toolchain.latest) {
+        for toolchain in installed {
+            let is_default = match default {
+                Some(ref d) => {
+                    if toolchain.latest {
+                        d == "latest"
+                    } else {
+                        d == &toolchain.version
+                    }
+                }
+                None => false,
+            };
+
+            let tag = match (is_default, toolchain.latest) {
                 (true, true) => Some(format!(" (latest, {})", console::style("default").blue())),
                 (true, false) => Some(format!(" ({})", console::style("default").blue())),
                 (false, true) => Some(" (latest)".to_string()),
@@ -22,6 +37,10 @@ pub async fn execute(_: Args) -> miette::Result<()> {
 
             println!("  {}{}", toolchain.version, tag.unwrap_or_default());
         }
+
+        let active = resolve::detect_active_toolchain();
+        let t = InstalledToolchain::from_path(&active)?;
+        println!("\nActive toolchain: {}", console::style(t.version).green());
     }
 
     Ok(())
--- src/toolchain/mod.rs
+++ src/toolchain/mod.rs
@@ -1,11 +1,10 @@
+use std::path::Path;
+
 pub mod index;
 pub mod package;
 pub mod resolve;
 
 pub struct InstalledToolchain {
-    /// Whether the installed toolchain is marked as default
-    pub default: bool,
-
     /// Whether this toolchain is installed as 'latest'
     pub latest: bool,
 
@@ -13,42 +12,39 @@ pub struct InstalledToolchain {
     pub version: String,
 }
 
+impl InstalledToolchain {
+    pub fn from_path(path: &Path) -> miette::Result<Self> {
+        let n = path
+            .file_name()
+            .map(|n| n.to_ascii_lowercase().to_string_lossy().to_string())
+            .ok_or_else(|| miette::miette!("Failed to get toolchain version"))?;
+        let latest = n == "latest";
+        let version = match latest {
+            false => n,
+            true => std::fs::read_to_string(path.join("version"))
+                .ok()
+                .map(|s| s.trim().to_string())
+                .unwrap_or_else(|| "latest".to_string()),
+        };
+
+        Ok(Self { latest, version })
+    }
+}
+
 pub fn installed_toolchains() -> miette::Result<Vec<InstalledToolchain>> {
     let toolchains_dir = crate::moonup_home().join("toolchains");
-    let default_version = resolve::detect_default_version();
 
     let toolchains = match toolchains_dir.read_dir() {
         Err(e) if e.kind() == std::io::ErrorKind::NotFound => vec![],
         Err(e) => return Err(miette::miette!(e).wrap_err("Failed to read toolchains directory")),
-        Ok(read_dir) => read_dir
-            .filter_map(std::io::Result::ok)
-            .filter_map(|e| {
-                let path = e.path();
-                let version = path.file_name().map(|n| {
-                    let n = n.to_ascii_lowercase();
-                    let latest = n == "latest";
-                    let default = match default_version.as_deref() {
-                        Some(v) => v == n,
-                        None => false,
-                    };
-                    let version = match latest {
-                        false => n.to_string_lossy().to_string(),
-                        true => std::fs::read_to_string(path.join("version"))
-                            .ok()
-                            .map(|s| s.trim().to_string())
-                            .unwrap_or_else(|| "latest".to_string()),
-                    };
-
-                    InstalledToolchain {
-                        default,
-                        latest,
-                        version,
-                    }
-                });
-
-                version
-            })
-            .collect::<Vec<_>>(),
+        Ok(read_dir) => {
+            let mut t = read_dir
+                .filter_map(std::io::Result::ok)
+                .filter_map(|e| InstalledToolchain::from_path(&e.path()).ok())
+                .collect::<Vec<_>>();
+            t.sort_by_key(|t| t.version.clone());
+            t
+        }
     };
 
     Ok(toolchains)
