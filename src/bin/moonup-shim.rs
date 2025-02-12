bdfe90fc19970c1111b50fc882f342bb15b1f8ab
Chawye Hsu <su+git@chawyehsu.com>
2025-02-12 11:24:13 +0800

    fix: spread toolchain spec down via env var
    a workaround for fixing #81
    
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- src/bin/moonup-shim.rs
+++ src/bin/moonup-shim.rs
@@ -5,6 +5,7 @@ use std::process::Command;
 
 use moonup::constant::RECURSION_LIMIT;
 use moonup::toolchain::resolve::detect_active_toolchain;
+use moonup::{moon_home, moonup_home};
 
 pub fn main() {
     if let Err(err) = run() {
@@ -20,7 +21,7 @@ fn run() -> Result<()> {
     let args = env::args_os().collect::<Vec<_>>();
     let current_exe = env::current_exe()
         .unwrap_or_else(|_| PathBuf::from(&args[0]))
-        .with_extension("");
+        .with_extension(""); // ensure `.exe` is removed
     let current_exe_name = current_exe
         .file_name()
         .and_then(|name| name.to_str())
@@ -37,9 +38,15 @@ fn run() -> Result<()> {
     let args_1 = args.get(1).and_then(|arg| arg.to_str());
     let args_1_is_toolchain = args_1.map_or(false, |arg| arg.starts_with('+'));
 
+    // Find the active toolchain:
+    // - If the first argument is a toolchain spec, use it.
+    // - If the `MOONUP_TOOLCHAIN_SPEC` environment variable is set, use it.
+    // - Otherwise, detect the active toolchain.
     let active_toolchain_root = if args_1_is_toolchain {
         let version = args_1.expect("has arg version").strip_prefix('+').unwrap();
-        moonup::moonup_home().join("toolchains").join(version)
+        moonup_home().join("toolchains").join(version)
+    } else if let Some(toolchain_spec) = env::var_os("MOONUP_TOOLCHAIN_SPEC") {
+        moonup_home().join("toolchains").join(toolchain_spec)
     } else {
         detect_active_toolchain()
     };
@@ -50,7 +57,7 @@ fn run() -> Result<()> {
         let version = active_toolchain_root
             .file_name()
             .and_then(|v| v.to_str())
-            .expect("Cannot get active toolchain version");
+            .expect("should get active toolchain version");
 
         println!("toolchain version '{version}' not installed");
 
@@ -65,13 +72,33 @@ fn run() -> Result<()> {
         }
     }
 
-    let actual_exe = active_toolchain_root.join("bin").join(current_exe_name);
+    let exe_relative_path = current_exe
+        .strip_prefix(moon_home())
+        .expect("should extract exe path");
+
+    let actual_exe = active_toolchain_root.join(exe_relative_path);
+    // println!("Running '{}'", actual_exe.display());
     let actual_libcore = active_toolchain_root.join("lib").join("core");
 
     let mut cmd = Command::new(actual_exe);
     let idx = if args_1_is_toolchain { 2 } else { 1 };
     cmd.args(args[idx..].iter().cloned());
     cmd.env("MOONUP_RECURSION_COUNT", (recursion_count + 1).to_string());
+
+    // NOTE(chawyehsu): It is not ideal and hacky to store the toolchain spec
+    // in an extra environment variable, but it is the only way to spread the
+    // toolchain spec to the child shim processes without requiring upstream
+    // changes in MoonBit build system... All of these are because of and for
+    // the weak isolation of the MoonBit toolchain...
+    cmd.env(
+        "MOONUP_TOOLCHAIN_SPEC",
+        env::var_os("MOONUP_TOOLCHAIN_SPEC").unwrap_or(
+            active_toolchain_root
+                .file_name()
+                .expect("should have toolchain version")
+                .to_os_string(),
+        ),
+    );
 
     if current_exe_name == "moon" {
         // intercept `moon upgrade` and proxy it to `moonup upgrade`
